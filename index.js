a78235563f2596f39b434628adfcadae227268d6
Aimable <aimable.niyogakiza@drpgroup.com>
2019-08-07 22:08:26 +0100

    Setup

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import { AppRegistry } from 'react-native';
+import Entrypoint from './app/Entrypoint';
+import { name as taskrunner2019 } from './app.json';
+
+AppRegistry.registerComponent(taskrunner2019, () => Entrypoint);
